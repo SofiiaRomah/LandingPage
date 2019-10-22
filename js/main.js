ff96e761a975c08ded7cf3d2f7489a72d0d897dc
Sofiia Romah <sofija.romah@gmail.com>
2019-10-22 20:21:21 +0300

    Added Contact Us form and footer

--- js/main.js
+++ js/main.js
@@ -4,43 +4,6 @@ $(document).ready(function () {
         $('.animated-navbar-toggler-icon').toggleClass('open');
     });
 
-    // animation to counters
-    var $window = $(window);
-    var $elem = $('#about');
-    var counterFinished = true;
-
-    function isScrolledIntoView($elem, $window) {
-        var docViewTop = $window.scrollTop();
-        var docViewBottom = docViewTop + $window.height();
-
-        var elemTop = $elem.offset().top;
-        var elemBottom = elemTop + $elem.height();
-
-        var isVisible = ((elemBottom > docViewTop) && (elemTop < docViewBottom));
-        return isVisible;
-    }
-
-    var wasVisible = isScrolledIntoView($elem, $window);
-
-    $(document).on("scroll", function () {
-        var isVisible = isScrolledIntoView($elem, $window);
-        if (!wasVisible && isVisible && counterFinished) {
-            counterFinished = false;
-            $('.counter-count').each(function () {
-                $(this).prop('Counter', 0).animate({
-                    Counter: $(this).text()
-                }, {
-                        duration: 2000,
-                        easing: 'swing',
-                        step: function (now) {
-                            $(this).text(Math.ceil(now));
-                        }
-                    });
-            }).promise().done(function () { counterFinished = true; });
-        }
-        wasVisible = isVisible;
-    });
-
     // Smooth scroll
     $('.nav-link, .navbar-brand, #welcomeBtn').on('click', function (event) {
         event.preventDefault();
@@ -50,5 +13,8 @@ $(document).ready(function () {
             scrollTop: $(href).offset().top
         }, 800);
     });
+
+    //animations
+    AOS.init();
 });
 
